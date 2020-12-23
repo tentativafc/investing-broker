9ee7cd9de5c7384383d3f1b00bef88afb0bd4761
Marcelo Ortiz de Santana <tentativafc@gmail.com>
2020-12-22 23:42:16 -0300

    Logout

--- app/frontend/app/src/components/Home.vue
+++ app/frontend/app/src/components/Home.vue
@@ -3,6 +3,9 @@
         <div class="container">
             <div class="row" v-if="isAuthenticated" >
                 <h1>Logged Area</h1>
+                <div class="col-6">  
+                    <a href="#" class="btn-lg" v-on:click="logout">Logout</a> 
+                </div>
             </div>  
             <div class="row" v-if="!isAuthenticated">
                 <div class="col-6">       
@@ -11,9 +14,9 @@
                 <div class="col-6">  
                     <a href="/register" class="btn-lg">Register</a> 
                 </div>
+                
             </div>  
-        </div>  
-
+        </div>
        UserData {{user}}
     </div>
 </template>
@@ -21,11 +24,21 @@
 
 <script>
 
-import { mapGetters } from "vuex";
+import { mapGetters, mapActions } from "vuex";
+
 
 export default {
     computed: {
         ...mapGetters(["isAuthenticated", "user"])
-    }
+    },
+    methods: {
+    ...mapActions([
+      'logout'
+    ]),
+    ...mapActions({
+      exit: 'logout'
+    })
+  }
+
 }
 </script>
