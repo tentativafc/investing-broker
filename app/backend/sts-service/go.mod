b9507d4074b8153dc83680c5920d452deb5adc8d
Marcelo Ortiz de Santana <tentativafc@gmail.com>
2021-01-06 08:31:49 -0300

    Sts repo

--- app/backend/sts-service/go.mod
+++ app/backend/sts-service/go.mod
@@ -5,10 +5,15 @@ go 1.15
 require (
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/golang/protobuf v1.4.3
+	github.com/jackc/pgproto3/v2 v2.0.7 // indirect
+	golang.org/x/crypto v0.0.0-20201221181555-eec23a3978ad // indirect
 	golang.org/x/net v0.0.0-20201224014010-6772e930b67b // indirect
 	golang.org/x/sys v0.0.0-20210105210732-16f7687f5001 // indirect
 	golang.org/x/text v0.3.4 // indirect
+	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
 	google.golang.org/genproto v0.0.0-20210105202744-fe13368bc0e1 // indirect
 	google.golang.org/grpc v1.34.0
 	google.golang.org/protobuf v1.25.0
+	gorm.io/driver/postgres v1.0.6 // indirect
+	gorm.io/gorm v1.20.9 // indirect
 )
---
+++ app/backend/sts-service/repo/repo.go
@@ -0,0 +1,64 @@
+package repo
+
+import (
+	"time"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+type ClientCredentials struct {
+	ID           string `gorm:"primarykey"`
+	ClientName   string `gorm:"not null"`
+	ClientId     string `gorm:"not null"`
+	ClientSecret string `gorm:"not null"`
+	CreatedAt    time.Time
+	UpdatedAt    time.Time
+}
+
+func (ClientCredentials) TableName() string {
+	return "client_credentials"
+}
+
+type ClientCredentialsRepository struct {
+	db *gorm.DB
+}
+
+func (ccr ClientCredentialsRepository) CreateClientCredentials(cr ClientCredentials) (ClientCredentials, error) {
+	err := ccr.db.Create(&cr).Error
+	if err != nil {
+		return ClientCredentials{}, err
+	}
+	return cr, nil
+}
+
+func (ccr ClientCredentialsRepository) UpdateClientCredentials(cr ClientCredentials) (ClientCredentials, error) {
+	err := ccr.db.Updates(&cr).Error
+	if err != nil {
+		return ClientCredentials{}, err
+	}
+	return cr, nil
+}
+
+func (cr ClientCredentialsRepository) FindByClientId(clientId string) (ClientCredentials, error) {
+	var cr ClientCredentials
+	err := cr.db.Where("client_id = ?", ClientId).First(&cr).Error
+	return cr, err
+}
+
+func NewClientCredentialsRepository() ClientCredentialsRepository {
+	db := DbInit()
+	ur := ClientCredentialsRepository{db: db}
+	return ur
+}
+
+func DbInit() *gorm.DB {
+	dbConfigs := "host=localhost user=postgres password=123456 dbname=postgres port=5432"
+	var err error
+	db, err := gorm.Open(postgres.Open(dbConfigs), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
+	db.AutoMigrate(&ClientCredentials{})
+	return db
+}
