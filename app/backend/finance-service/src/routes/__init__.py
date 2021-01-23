0c57352da00e2446e4cd886d3b98c4baea2f1c84
Marcelo Ortiz <tentativafc@gmail.com>
2021-01-23 12:34:13 -0300

    Quotes endpoint

--- app/backend/finance-service/src/routes/__init__.py
+++ app/backend/finance-service/src/routes/__init__.py
@@ -1,16 +1,45 @@
 # coding=utf-8
 __author__ = 'Marcelo Ortiz'
 
-from flask_restful import Resource, Api
+
+from flask import request
+from flask_restful import Resource, Api, reqparse, abort
+from marshmallow import Schema, fields
+import re
+
+from datetime import datetime
+import pandas as pd
+import MetaTrader5 as mt5
+from ortisan_ta.dataaccess import DataItem, MetaTraderDataAccess
+import ortisan_ta.utils.analysis as ortisan_ta
+
+class QuotesSchema(Schema):
+    symbol = fields.Str(required=True)
+    init_date = fields.Date(required=True)
+    final_date = fields.Date(required=True)    
+
+class Quotes(Resource):
+    def __init__(self):
+        self.schema = QuotesSchema()
+        self.data_access = MetaTraderDataAccess()
+
+    def get(self):
+        errors = self.schema.validate(request.args)
+        if errors:            
+            abort(400, message="Invalid parameters", cause=str(errors))   
+        else:
+            search_filter = self.schema.load(request.args)
+            df = self.data_access.get_rates_from_symbol(search_filter['symbol'], pd.Timestamp(search_filter['init_date']), pd.Timestamp(search_filter['final_date']), mt5.TIMEFRAME_D1)
+            # to_json() does not serialize the dataframe index
+            df_with_date = df.reset_index()
+            df_with_date['Date'] = df.index
+            return df_with_date.to_json(orient='records', date_format='iso', force_ascii=False, lines=True)
+
 
 class Portifolio(Resource):
     def get(self):
         return {'hello': 'world'}
 
-class Quotes(Resource)
-    def get(self):
-        return {'hello': 'world'}
-
-class Order(Resource)
+class Order(Resource):
     def get(self):
         return {'hello': 'world'}
