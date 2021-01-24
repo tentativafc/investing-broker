d219241b3e200f129f5ee6359a352535ae4c11da
Marcelo Ortiz <tentativafc@gmail.com>
2021-01-24 12:54:29 -0300

    Error handler.

--- app/backend/finance-service/src/routes/__init__.py
+++ app/backend/finance-service/src/routes/__init__.py
@@ -6,6 +6,7 @@ from flask import request
 from flask_restful import Resource, Api, reqparse, abort, marshal_with
 from marshmallow import Schema, fields
 from dtos import QuoteDto, PortfolioDto
+from errors import IllegalArgumentException
 
 from datetime import datetime
 import pandas as pd
@@ -23,13 +24,15 @@ class Quotes(Resource):
 
     @marshal_with(QuoteDto.FIELDS)
     def get(self):
-        errors = self.getSchema.validate(request.args)
-        if errors:            
-            abort(400, message="Invalid parameters", cause=str(errors))   
-        else:
-            search_filter = self.getSchema.load(request.args)
-            return self.business.getQuotes(search_filter['symbol'], pd.Timestamp(search_filter['init_date']), pd.Timestamp(search_filter['final_date']))
-
+        try:
+            errors = self.getSchema.validate(request.args)
+            if errors:            
+                abort(400, message="Invalid parameters", cause=str(errors))   
+            else:
+                search_filter = self.getSchema.load(request.args)
+                return self.business.getQuotes(search_filter['symbol'], pd.Timestamp(search_filter['init_date']), pd.Timestamp(search_filter['final_date']))
+        except Exception as exc:            
+            abort(500, message="Error to get quotes.", cause=exc)
 
 class PortfolioGetSchema(Schema):
     amount_assets = fields.Integer(required=True)
@@ -41,12 +44,19 @@ class Portfolio(Resource):
 
     @marshal_with(PortfolioDto.FIELDS)
     def get(self):
-        errors = self.getSchema.validate(request.args)
-        if errors:            
-            abort(400, message="Invalid parameters", cause=str(errors))   
-        else:
-            search_filter = self.getSchema.load(request.args)
-            return self.business.createPortfolio(search_filter['amount_assets'])
+        try:
+            errors = self.getSchema.validate(request.args)
+            if errors:            
+                abort(400, message="Invalid parameters", cause=str(errors))   
+            else:
+                search_filter = self.getSchema.load(request.args)
+                return self.business.createPortfolio(search_filter['amount_assets'])
+        except Exception as exc:
+            if isinstance(exc, IllegalArgumentException):                    
+                print(dir(exc))
+                abort(400, message=str(exc))
+            else:
+                abort(500, message="Error to create portfolio.", cause=exc)
 
 class Order(Resource):
     def get(self):
